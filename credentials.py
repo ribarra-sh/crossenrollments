fa873f4a2196064a44d2a3fba5a3cd4593c49311
ribarra-sh <146129368+ribarra-sh@users.noreply.github.com>
2023-12-08 16:00:06 -0800

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -2,12 +2,12 @@ import os
 
 def get_credentials():
     return  [
-      '54.183.34.45', # IP del EC2
-      'ribarra', # User del Ec2 (egarcia o vgarcia)
+      '', # IP del EC2
+      '=', # User del Ec2 (egarcia o vgarcia)
       f'{os.getcwd()}/sos.pem', 
       'sos-prod-instance-1-read1.clj3rhohf9hn.us-west-1.rds.amazonaws.com', # URL de la DB
       3306,
-      'ribarra', #egarcia o vgarcia (user de la DB)
-      '2a7phQn3ZFcfhvcZhBp6GX89gBWXhgaJ', # password
+      '', #egarcia o vgarcia (user de la DB)
+      '', # password
       'sh2_online_sales' # sh2_online_sales
       ]
